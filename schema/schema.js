cf1258b568430c5a6afa76d6f790f551c74a3b7e
Bart Kalanski <bkalanski@gmail.com>
2021-05-23 21:37:48 +0100

    Add new graphQl object type

--- schema/schema.js
+++ schema/schema.js
@@ -1,39 +1,73 @@
 const graphql = require('graphql')
 const axios = require('axios')
-const { 
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
 } = graphql
- 
+
 // schema - what tells the GraphQL what type of data that we're working with and how all the relations play out between those different pieces of data
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((res) => res.data)
+      },
+    },
+  }),
+})
 
 const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: {
-        id: { type: GraphQLString },
-        firstName: { type: GraphQLString },
-        age: { type: GraphQLInt },
-    }
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data)
+      },
+    },
+  }),
 })
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        user: {
-            type: UserType,
-            args: { id: { type: GraphQLString }},
-            resolve(parentValue, args){
-              // request to json server
-              return axios.get(`http://localhost:3000/users/${args.id}`)
-                .then(resp => resp.data)
-                
-            }
-        }
-    }
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        // request to json server
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data)
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((resp) => resp.data)
+      },
+    },
+  },
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 })
